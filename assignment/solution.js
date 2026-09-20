ed6a5bb1e2723a40e7a02163721f55eeb89cfc56
Ajoe <ajoe8@student.giu-uni.de>
2026-09-20 19:57:03 +0300

    Complete assignment

---
+++ assignment/solution.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const heroName = "Ajoe the Warrior";
+const studentId = "12345678";
+const power = Number(studentId) * 7;
+const warcry = "By the Nile, the Sand Kraken falls!";
+
+console.log(heroName);
+console.log(studentId);
+console.log(power);
+console.log(warcry);
